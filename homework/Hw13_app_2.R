57e0171a6d185fbab57f9372fb75cca9bb799381
aahuman <clismith@ucdavis.edu>
2024-03-06 21:03:45 -0800

    hw 13

---
+++ homework/Hw13_app_2.R
@@ -0,0 +1,45 @@
+library(tidyverse)
+library(shiny)
+library(shinydashboard)
+library(naniar)
+
+UC_admit <- read_csv("data/UC_admit.csv")
+
+ui <- dashboardPage(
+  dashboardHeader(title = "UC Campus Admissions by Year 2010-2019"),
+  dashboardSidebar(disable=T),
+  dashboardBody(
+    fluidRow(
+      box(title = "Plot Settings", width = 3,
+          selectInput("x", "Select Ethnicity", choices = c("African American", "Asian", "Chicano/Latino", "International", "White", "Uknown"), 
+                      selected = "African American"),
+          selectInput("y", "Select Campus", choices = c("Davis", "Irvine", "Berkeley", "Irvine", "Los_Angeles", "Merced", "Riverside", "San_Diego", "Santa_Barbara", "Santa_Cruz"),
+                      selected = "Davis"),
+          
+          selectInput("z", "Select Admit Category", choices = c("Applicants", "Admits", "Enrollees"),
+                      selected = "Applicants")
+      ), 
+      box(title = "UC Admissions", width = 8,
+          plotOutput("plot", width = "600px", height = "500px")
+      ) 
+    ) 
+  )
+)
+server <- function(input, output, session) {
+  
+  session$onSessionEnded(stopApp)
+  
+  output$plot <- renderPlot({
+    
+    UC_admit %>% 
+      filter(Ethnicity==input$x, Campus==input$y, Category==input$z) %>% 
+      filter(FilteredCountFR!="NA", Ethnicity!="All") %>% 
+      ggplot(aes(x=reorder(Academic_Yr, FilteredCountFR), y=FilteredCountFR))+
+      geom_col(color="black", fill="dodgerblue2", alpha=0.4) +
+      theme_linedraw(base_size = 18) +
+      theme(axis.text.x = element_text(angle = 60, hjust = 1))+
+      labs(x = "academic year", y = "number")
+  })
+}
+
+shinyApp(ui, server)
